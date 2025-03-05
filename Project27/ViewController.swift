92245085e8a34382c0e5ad4b769ed0273660455e
retessam7 <retessam7@gmail.com>
2025-03-05 01:05:30 +0100

    //

--- Project27/ViewController.swift
+++ Project27/ViewController.swift
@@ -32,6 +32,8 @@ class ViewController: UIViewController {
             drawCircle()
         case 2:
             drawRotatedSquares()
+        case 3:
+            drawLines()
         default:
             break
         }
@@ -90,6 +92,35 @@ class ViewController: UIViewController {
                 ctx.cgContext.rotate(by: CGFloat(amount))
                 // we need to draw our rotated squares so they are centered on our center:
                 ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = img
+    }
+    
+    func drawLines() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0 ..< 256 {
+                ctx.cgContext.rotate(by: .pi / 2)
+                
+                if first {
+                    ctx.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    ctx.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                
+                length *= 0.99
             }
             
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
